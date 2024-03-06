0639629ac6c9475dd90297366b6c6b5732e80f4f
Rainbow4 <becoool2013@mail.ru>
2024-03-06 14:29:35 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from aiogram import Bot, Dispatcher
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+token = os.getenv('TOKEN')
+admin_id = os.getenv('ADMIN_ID')
+# переменные дл взаимодейств с ботом
+bot = Bot(token=token, parse_mode='HTML')
+dp = Dispatcher()
+
+
+# start_bot = оповещает ADMIN о запуске
+async def start_bot(bot: Bot):
+    await bot.send_message(admin_id, text='Бот был запущен')
+
+
+dp.startup.register(start_bot)
+
+
+async def start():
+    try:
+        await dp.start_polling(bot, skip_updates=True)
+    finally:
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(start())
